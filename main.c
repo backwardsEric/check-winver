40faaa9693b3deef72cbd788ab776a1c0a9e73b0
Eric Branlund <ebranlund@fastmail.com>
2024-08-24 18:29:34 -0600

    Check handling of WINVER and _WIN32_WINNT in different environments

---
+++ main.c
@@ -0,0 +1,18 @@
+#ifndef WIN32_CONSOLE_MODE
+#define WIN32_CONSOLE_MODE
+#endif
+#ifndef WINVER
+#define WINVER (0x0500)
+#elif WINVER < 0x0500
+#undef WINVER
+#define WINVER (0x0500)
+#endif
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	(void)printf("WINVER is %lx\n", (unsigned long)WINVER);
+	(void)printf("_WIN32_WINNT is %lx\n", (unsigned long)_WIN32_WINNT);
+	return 0;
+}
